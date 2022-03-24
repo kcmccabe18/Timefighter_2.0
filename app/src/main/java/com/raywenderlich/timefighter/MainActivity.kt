d237c43ebe1ac13c1992e8579e713406e93d8035
Kerry McCabe <kmccab6@live.spcollege.edu>
2022-03-24 17:01:30 -0400

    ch5 and app finished

--- app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,7 +40,11 @@ class MainActivity : AppCompatActivity() {
 
     tapMeButton = findViewById(R.id.tap_me_button)
 
-    tapMeButton.setOnClickListener { incrementScore() }
+    tapMeButton.setOnClickListener { view ->
+      val bounceAnimation = AnimationUtils.loadAnimation(this, R.anim.bounce)
+      view.startAnimation(bounceAnimation)
+        incrementScore()
+    }
 
     if (savedInstanceState != null) {
       score = savedInstanceState.getInt(SCORE_KEY)
@@ -63,6 +71,20 @@ class MainActivity : AppCompatActivity() {
     super.onDestroy()
 
     Log.d(TAG, "onDestroy called.")
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    super.onCreateOptionsMenu(menu)
+    menuInflater.inflate(R.menu.menu, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    if (item.itemId == R.id.about_item) {
+      showInfo()
+    }
+    return true
   }
 
   private fun incrementScore(){
@@ -133,6 +155,16 @@ class MainActivity : AppCompatActivity() {
     Toast.makeText(this, getString(R.string.game_over_message, score),
       Toast.LENGTH_LONG).show()
     resetGame()
+  }
+
+  private fun showInfo() {
+    val dialogTitle = getString(R.string.about_title, BuildConfig.VERSION_NAME)
+    val dialogMessage = getString(R.string.about_message)
+
+    val builder = AlertDialog.Builder(this)
+    builder.setTitle(dialogTitle)
+    builder.setMessage(dialogMessage)
+    builder.create().show()
   }
 
   companion object {
